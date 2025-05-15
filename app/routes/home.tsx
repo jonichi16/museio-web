62904c58ef9f84347e6d39cb038bef7115aed525
jonichi <jonichi16@gmail.com>
2025-05-16 01:22:27 +0800

    feat: add shadcn/ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { Button } from "~/lib/components/ui/button";
+
 export function meta() {
   return [
     { title: "Museio" },
@@ -6,9 +9,31 @@ export function meta() {
 }
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount((prevState) => ++prevState);
+  };
+
+  const handleDecrease = () => {
+    setCount((prevState) => --prevState);
+  };
+
   return (
-    <main>
-      <h1>Museio App</h1>
+    <main className="min-h-screen flex flex-col gap-5 items-center justify-center">
+      <h1 className="text-6xl font-bold uppercase">Museio App</h1>
+      <p className="text-2xl">{count}</p>
+      <Button className="font-bold" onClick={handleIncrease}>
+        Increase
+      </Button>
+      <Button
+        variant="secondary"
+        size="lg"
+        className="font-bold"
+        onClick={handleDecrease}
+      >
+        Decrease
+      </Button>
     </main>
   );
 }
